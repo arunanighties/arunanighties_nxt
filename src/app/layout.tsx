cdf948eb0e66cf2faf8ecb7187f38754cf9589fa
nagendradurgaprasadilla <nagendradurgaprasadilla@gmail.com>
2026-05-20 17:50:56 +0530

    chore: configure website favicon from logo.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
-        <link rel="icon" type="image/icon" href="/favicon.ico" />
       </head>
       <body>
         <div id="root">{children}</div>
